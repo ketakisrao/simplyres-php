b398ea634d6fc0d4938e0a15622f0f9221adc55c
Ketaki Rao <ketakisrao@outlook.com>
2015-12-19 20:02:29 +0530

    First commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,10 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-
+// allow origin
+header('Access-Control-Allow-Origin: *');
+// add any additional headers you need to support here
+header('Access-Control-Allow-Headers: Origin, Content-Type');
 $app = new Illuminate\Foundation\Application(
 	realpath(__DIR__.'/../')
 );
