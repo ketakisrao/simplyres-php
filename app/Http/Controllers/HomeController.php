4dce900c3b4fea2c7483a3ea7548898260b084e1
Ketaki Rao <ketakisrao@outlook.com>
2015-12-20 15:55:55 +0530

    Check bookings page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller {
     
     public function bookings()
     {
-        $bookings = Booking::all();
+        $bookings = Booking::where('username', 'Ketaki Rao');
         return view('bookings',['bookings' => $bookings]);
     }
 
