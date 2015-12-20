79b3b0af869cb3cec037e028cc08623012704096
Ketaki Rao <ketakisrao@outlook.com>
2015-12-20 20:20:44 +0530

    Check bookings page

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -1,31 +1,32 @@
 @extends('app')
 
 @section('content')
-<H1>Congratulations on your first Laravel page!!</H1>
   <div class="container" >
-    <div class="table-responsive">          
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Restaurant Name</th>
-            <th>Location</th>
-            <th>Date</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach($bookings as $booking)
-          <tr>
-            <td>{{$booking->restaurant_name}}</td>
-            <td>{{$booking->location}}</td>
-            <td>{{$booking->date}}</td>
-            <td>{{$booking->time}}</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-
-    <p>You do not have any bookings, <strong>Make your first one now by clicking on dashboard!</strong><p>
+      @if($bookings)
+            <div class="table-responsive">          
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Restaurant Name</th>
+                    <th>Location</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($bookings as $booking)
+                  <tr>
+                    <td>{{$booking->restaurant_name}}</td>
+                    <td>{{$booking->location}}</td>
+                    <td>{{$booking->date}}</td>
+                    <td>{{$booking->time}}</td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+      @else
+            <p>You do not have any bookings, <strong>Make your first one now by clicking on dashboard!</strong></p>
+      @endif
     </div>
 @endsection
