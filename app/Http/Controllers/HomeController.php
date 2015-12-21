c0403c19ffb9b19c501363eebd67aa41395b9a0b
Ketaki Rao <ketakisrao@outlook.com>
2015-12-22 04:40:48 +0530

    check twitter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends Controller {
         return redirect('bookings');
     }
     
-    public function display_bookings()
+    public function displayBookings()
     {
         $bookings = Booking::all();
         return Response::json(array('Bookings' => $bookings, 'status_code'=> 200));
