cbf809fc04a848b7e99f3f2aeb89c688d8f72ac6
Ketaki Rao <ketakisrao@outlook.com>
2015-12-21 21:04:22 +0530

    check form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller {
     
     public function bookings()
     {
-        $username = "Ketaki Rao";//Auth::user()->name;
+        $username = \Auth::user()->name;
         $bookings = Booking::where('username', $username)
                ->take(10)
                ->get();
