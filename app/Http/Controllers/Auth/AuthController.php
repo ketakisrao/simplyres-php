eb35c8158e5931a0eab6c01be0ab3c3c1be9fd01
Ketaki Rao <ketakisrao@outlook.com>
2015-12-22 03:27:32 +0530

    check twitter

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,11 +1,10 @@
 <?php namespace App\Http\Controllers\Auth;
-
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
-use Auth;
-use Socialite;
+use Socialize;
 class AuthController extends Controller {
 
 	/*
