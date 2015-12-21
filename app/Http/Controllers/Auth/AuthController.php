c58fae9d3940a747676d0f0095a695c8582ef179
Ketaki Rao <ketakisrao@outlook.com>
2015-12-22 03:37:37 +0530

    check twitter

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -54,12 +54,7 @@ class AuthController extends Controller {
      */
     public function handleProviderCallback()
     {
-        try {
-            $user = Socialize::driver('twitter')->user();
-        } catch (Exception $e) {
-            return redirect('auth/twitter');
-        }
- 
+        $user = Socialize::with('twitter')->user();
         $authUser = $this->findOrCreateUser($user);
  
         Auth::login($authUser, true);
